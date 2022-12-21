d9f85e2e490f407f70d650974e9b4a70e20779f0
Lotacodic <Ngenejoshua05@gmail.com>
2022-12-21 13:35:05 +0100

    modify files

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-### An Ecommerce Console Application that allows user to pick any product needed from the catalog, displays the price of total item(s) chosen, and also checkout section.
+## An Ecommerce Console Application 
+
+This project is a simple E-commerce console App that allows user to pick any product needed from the catalog, displays the price of total item(s) chosen, and also checkout the user.
+
+
+### Usage
+- To use this project `clone` this it with this command below or any preferred method, cd into the `animal-kingdom-model` folder
+```
+git clone https://github.com/lotacodic/ecommerce-console-application.git
+```
+### Note
+- `node js` is needed to run this project on terminal 
+- recommended IDE is Vs Code, I recommend Maximum Panel Size for a better view of the output
+- run this command `node script.js` on the terminal of your Vs Code. 
+
+### Alternative
+- You can as well run this code on replit: <a href="https://replit.com/@joshuangene/ecommerceConsoleApp#index.js"> Replit Link</a>
+
+### Sample
+  - Below is the expected result when you run `node script.js` on your terminal.
+
+  <img src="image\Screenshot (221).png"/>
--- script.js
+++ script.js
@@ -1,53 +1,157 @@
 //catalog
 const cataLog = [
-    ["Lenovo ThinkPad 13", 600],
-    ["Apple Macbook Air", 900],
-    ["Apple Pro Retina", 900],
-    ["HP 13 corei3", 400],
-    ["HP 225 G4 AMD", 500],
-    ["Lenovo Flex10", 250]
+  ["lenovo thinkpad 13", 600],
+  ["apple macbook Air", 900],
+  ["apple retina pro", 900],
+  ["hp corei3", 400],
+  ["dell inspiron", 500],
+  ["lenovo flex", 250],
 ];
 
-//creating a function to replicate console.log 
+//creating a function to replicate console.log
 function print(message) {
-    console.log(message);
-};
+  console.log(message);
+}
 
-//function to display items on cataLog
+//function to display lists of items and prices on cataLog
 function catlogList(items) {
-    var list = "\n*************************\n";
-    for(i = 0; i < items.length; i++) {
-        list +="\n["+ items[i][0] +" costs "+ "$" + items[0][1] +"]\n";
-    }
-        list += "\n*************************\n";
-        print(list);
+  var list = "\n***********************************\n";
+  for (i = 0; i < items.length; i++) {
+    list += "\n[" + items[i][0] + "," + " costs " + "$" + items[i][1] + "]\n";
+  }
+  list += "\n***********************************\n";
+  print(list);
+}
+
+// iCompleted: a function to loop through the prices
+/*
+const loopCatalogPrice = (loop) => {
+  let list = [];
+  for (let i = 0; i < loop.length; i++) {
+    list += loop[i][1];
+  }
+  print(list);
 };
+*/
 
-var cart = []; //Empty cart, user items goes here..
+//Empty cart, user items goes here..
+var cart = []; 
 
-//enabling prompt in console using readline module
-var r1 = require("readline"); //imports readline to js file
-//for receiving user input
+//enabling prompt in console using readline module 
+//accepts user input and store it a callback function
+var r1 = require("readline"); 
 var prompts = r1.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
-//Display welcome, prompt Messages 
-prompts.question("\nEnter your name: ", (username)=>{
-    print(`\nHey ${username}, welcome to Jostore`);
-    print(`\nWe sell different laptop brands\n\nyou make your selection, we do the delivery`);
-    print(`\n Below are the lists and cost of laptops in our stores: \n`);
-    catlogList(cataLog);
-    prompts.question("Select the devices you want to be added to your shopping cart: ", (itemSelected)=> {
-       if(Array.prototype.values(cataLog.indexOf(itemSelected) > -1)) {
-        print(`${itemSelected} has been added to your cart\n`);
+//Display welcome, prompts Messages in the interface
+prompts.question("\nEnter your name: ", (username) => {
+  print(`\nHey ${username},\n
+  ============ WELCOME TO JOSTORE ============`);
+  print("\n======= We sell different laptop brands=====\n\n====== You make your selection, we do the delivery======"
+  );
+  print(`\n============= VIEW OUR ITEMS!==================== \n`);
+  catlogList(cataLog);
+  //to enable the user to add one item from the lists
+  prompts.question(
+    "Enter the device you want to be added to your shopping cart \n(enter correctly, words in the lists):  ",
+    (itemSelected) => {
+      if (cataLog[0].includes(itemSelected)) {
+        print(`${itemSelected} has been added to your shopping cart\n`);
         cart.push(itemSelected);
         print(`Your shopping cart: \n${cart}`);
-       }
-       else{
-        print(`${itemSelected} not in our catalog!`)
-       }
-       print(`\nDo you wish to add more to your cart?`)
-})
-})
+      } else if (cataLog[1].includes(itemSelected)) {
+        print(`${itemSelected} has been added to your shopping cart\n`);
+        cart.push(itemSelected);
+        print(`Your shopping cart: \n${cart}`);
+      } else if (cataLog[2].includes(itemSelected)) {
+        print(`${itemSelected} has been added to your shopping cart\n`);
+        cart.push(itemSelected);
+        print(`Your shopping cart: \n${cart}`);
+      } else if (cataLog[3].includes(itemSelected)) {
+        print(`${itemSelected} has been added to your shopping cart\n`);
+        cart.push(itemSelected);
+        print(`Your shopping cart: \n${cart}`);
+      } else if (cataLog[4].includes(itemSelected)) {
+        print(`${itemSelected} has been added to your shopping cart\n`);
+        cart.push(itemSelected);
+        print(`Your shopping cart: \n${cart}`);
+      } else if (cataLog[5].includes(itemSelected)) {
+        print(`${itemSelected} has been added to your shopping cart\n`);
+        cart.push(itemSelected);
+        print(`Your shopping cart: \n${cart}`);
+      } else {
+        print(`${itemSelected} is not in stock now`);
+      }
+      //to enable the user to add more items from the lists
+      prompts.question(
+        "\nAdd more laptop to your shopping cart? \nor Hit 'Enter' to go to checkout:  ",
+        (addMoreItems) => {
+          if (cataLog[0].includes(addMoreItems)) {
+            print(`\n${addMoreItems} has been added to your shopping cart\n`);
+            cart.push(addMoreItems);
+            print(`Your shopping cart: \n ${cart}`);
+          } else if (cataLog[1].includes(addMoreItems)) {
+            print(`\n${addMoreItems} has been added to your shopping cart\n`);
+            cart.push(addMoreItems);
+            print(`Your shopping cart: \n ${cart}`);
+          } else if (cataLog[2].includes(addMoreItems)) {
+            print(`\n${addMoreItems} has been added to your shopping cart\n`);
+            cart.push(addMoreItems);
+            print(`Your shopping cart: \n ${cart}`);
+          } else if (cataLog[3].includes(addMoreItems)) {
+            print(`\n${addMoreItems} has been added to your shopping cart\n`);
+            cart.push(addMoreItems);
+            print(`Your shopping cart: \n ${cart}`);
+          } else if (cataLog[4].includes(addMoreItems)) {
+            print(`\n${addMoreItems} has been added to your shopping cart\n`);
+            cart.push(addMoreItems);
+            print(`Your shopping cart: \n ${cart}`);
+          } else if (cataLog[5].includes(addMoreItems)) {
+            print(`\n${addMoreItems} has been added to your shopping cart\n`);
+            cart.push(addMoreItems);
+            print(`Your shopping cart: \n ${cart}`);
+          } else {
+            print(`${addMoreItems} is not in stock now`);
+          }
+          prompts.question("Enter your Email Address: ", (emailAddress) => {
+            prompts.question("Enter your Home Address: ", (homeAddress) => {
+              prompts.question(
+                "Enter your Phone Number \n(make sure its 11): ",
+                (phoneNumber) => {
+                  let checkoutMessage = "";
+                  if (phoneNumber.length >= 10) {
+                    checkoutMessage =
+                      "\n\n Thank You! " +
+                      username +
+                      "\n\n Your details have been stored and expect delivery in 3 days" +
+                      "\n\n-----------------------------------------------" +
+                      "\n\n-----------------------------------------------" +
+                      "\n\n Username              : " +
+                      username +
+                      "\n\n Email                 : " +
+                      emailAddress +
+                      "\n\n Contact Address       : " +
+                      homeAddress +
+                      "\n\n Phone Number          : " +
+                      phoneNumber +
+                      `\n\n Shopping Cart - ${cart}   : ` +
+                      "\n\n Total                 : " +
+                      "\n\n-- Thank you for shopping at JOSTORE --" +
+                      "\n\n-----------------------------------------------";
+                  } else {
+                    checkoutMessage =
+                      `${phoneNumber} its not up to 11 digits,\n please
+                    restart and input 11  digits of your phone number `;
+                  }
+                  print(checkoutMessage);
+                }
+              );
+            });
+          });
+        }
+      );
+    }
+  );
+});
